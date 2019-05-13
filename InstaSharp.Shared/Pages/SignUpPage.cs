d02e0fd860d7b17771a0c485da715702f1cdcba5
RogerAnselmo <rogerio_anselmo@hotmail.com>
2019-05-13 17:50:05 -0300

    SignUp-Page (#4)

--- InstaSharp.Shared/Pages/SignUpPage.cs
+++ InstaSharp.Shared/Pages/SignUpPage.cs
@@ -1,15 +1,55 @@
 ﻿using InstaSharp.Shared.Pages.Base;
 using OpenQA.Selenium;
-using System;
 
 namespace InstaSharp.Shared.Pages
 {
     public class SignUpPage : LoggedOutBasePage
     {
-        public SignUpPage(IWebDriver driver) : base(driver)
+        private readonly FaceBookPage faceBookPage;
+        private readonly HomePage homePage;
+        private readonly LogInPage logInPage;
+
+        public SignUpPage(IWebDriver driver, FaceBookPage faceBookPage, HomePage homePage, LogInPage logInPage) : base(driver)
         {
+            this.faceBookPage = faceBookPage;
+            this.homePage = homePage;
+            this.logInPage = logInPage;
         }
 
-        protected override string UrlPath => throw new NotImplementedException();
+        protected override string UrlPath => "/accounts/emailsignup/";
+
+        public string MobileNumberOrEmail
+        {
+            get => driver.FindElement(By.Name("emailOrPhone")).GetAttribute("value");
+            set => SetText(By.Name("emailOrPhone"), value);
+        }
+
+        public string FullName
+        {
+            get => driver.FindElement(By.Name("fullName")).GetAttribute("value");
+            set => SetText(By.Name("fullName"), value);
+        }
+        public string UserName
+        {
+            get => driver.FindElement(By.Name("username")).GetAttribute("value");
+            set => SetText(By.Name("username"), value);
+        }
+        public string Password
+        {
+            get => driver.FindElement(By.Name("password")).GetAttribute("value");
+            set => SetText(By.Name("password"), value);
+        }
+
+        public HomePage SignUpButton()
+        {
+            driver.FindElement(By.XPath("//form//button[@type='submit']")).Click();
+            return homePage;
+        }
+
+        public FaceBookPage LoginWithFaceBookButton()
+        {
+            driver.FindElement(By.XPath("//form//button[./span[contains(@class, 'Facebook')]]")).Click();
+            return faceBookPage;
+        }
     }
 }
