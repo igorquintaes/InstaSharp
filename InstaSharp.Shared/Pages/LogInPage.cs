3f979fc718bc96815eaa9aab7439bd8380d59f34
Rogério Anselmo <rogerio.anselmo@lambda3.com.br>
2019-05-13 14:50:21 -0300

    wip

--- InstaSharp.Shared/Pages/LogInPage.cs
+++ InstaSharp.Shared/Pages/LogInPage.cs
@@ -7,8 +7,22 @@ namespace InstaSharp.Shared.Pages
     {
         protected override string UrlPath => "/";
 
-        public LogInPage(IWebDriver driver)
-            : base(driver)
-        { }
+        public LogInPage(IWebDriver driver) : base(driver) { }
+
+        public string username
+        {
+            get => driver.FindElement(By.Name("username")).Text;
+            set => SetText(By.Name("username"), value);
+        }
+
+        public string password
+        {
+            get => driver.FindElement(By.Name("password")).Text;
+            set => SetText(By.Name("password"), value);
+        }
+
+        public void LoginButton() => driver.FindElement(By.XPath("//form//button[@type='submit']")).Click();
+
+        public void LoginWithFaceBookButton() => driver.FindElement(By.XPath("//form//span[contains(@class, 'coreSpriteFacebookIcon')]")).Click();
     }
 }
