bff4998c1c7fba9c836ec2ac5aa7303016affd1a
igorquintaes <igor.quintaes@lambda3.com.br>
2019-05-15 17:07:20 -0300

    user content updated (#8)

--- InstaSharp.Shared/Pages/UserPage.cs
+++ InstaSharp.Shared/Pages/UserPage.cs
@@ -1,8 +1,12 @@
-﻿using InstaSharp.Shared.Extensions;
+﻿using HtmlAgilityPack;
+using InstaSharp.Shared.Enum;
+using InstaSharp.Shared.Extensions;
 using InstaSharp.Shared.Pages.Base;
 using InstaSharp.Shared.Pages.Modals;
 using OpenQA.Selenium;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace InstaSharp.Shared.Pages
@@ -10,14 +14,19 @@ namespace InstaSharp.Shared.Pages
     public class UserPage : LoggedInBasePage
     {
         private readonly UserFollowersModal userFollowersModal;
+        private readonly UserFollowingModal userFollowingModal;
 
         protected override string UrlPath => "/{0}/";
 
         public UserPage(
             IWebDriver driver,
-            UserFollowersModal userFollowersModal)
-        : base(driver) => 
+            UserFollowersModal userFollowersModal,
+            UserFollowingModal userFollowingModal)
+        : base(driver)
+        {
             this.userFollowersModal = userFollowersModal;
+            this.userFollowingModal = userFollowingModal;
+        }
 
         public int PostQuantity => Convert.ToInt32(
             driver.FindElement(By.XPath("//*[@role='main']//header//ul/li[1]/span/span"))
@@ -53,7 +62,44 @@ namespace InstaSharp.Shared.Pages
             return userFollowersModal;
         }
 
-        public void FollowingButton() => 
-            throw new NotImplementedException();
+        public UserFollowingModal FollowingButton()
+        {
+            var buttonXPath = "(//section//li)[3]/a";
+            driver
+                .FindElement(By.XPath(buttonXPath))
+                .AdjustElementExibition(driver)
+                .Click();
+
+            userFollowingModal.WaitForModal();
+            return userFollowingModal;
+        }
+
+        public IEnumerable<string> GetPostsLinks(int quantity)
+        {
+            var postQuantity = PostQuantity;
+
+            if (quantity > postQuantity)
+                quantity = postQuantity;
+
+            LoadPosts();
+            var document = new HtmlDocument();
+            document.LoadHtml(driver.PageSource);
+            
+            foreach(var postLink in document.DocumentNode.SelectNodes("//article//a"))
+                yield return postLink.GetAttributeValue("href", string.Empty);
+
+            void LoadPosts()
+            {
+                var elements = driver.FindElements(By.XPath("//article//a"));
+                if (elements.Count < quantity)
+                {
+                    elements.Last().ScrollElement(driver);
+                    driver.Wait().Until(x => x.FindElements(By.XPath("//article//a")).Count > elements.Count);
+                    LoadPosts();
+                }
+
+                return;
+            }
+        }
     }
 }
